8fef5c25c851960ccaa52c4d7b6857644d9d3c78
michaelw <michaelw@confetti.co.uk>
2012-07-17 15:21:26 +0100

    Compiled JS with coffee tool, and uses slightly different approach to aliasing jQuery. Closes GH-3

--- jquery.stick.coffee
+++ jquery.stick.coffee
@@ -1,41 +1,41 @@
-$ = jQuery
-STATIC = 'static'
-FIXED = 'fixed'
-$.fn.stick = (args) -> s = new Stick(this, args)
+jQuery ($) ->
+  STATIC = 'static'
+  FIXED = 'fixed'
+  $.fn.stick = (args) -> s = new Stick(this, args)
 
-class Stick
-  constructor: (@target, args) ->
-    @offset = if typeof args is "number" then args else 0
-    $.extend @, args if typeof args is "object"
-    @state = STATIC
-    @resize()
-    ((s) -> $(window).scroll(-> s.scroll(this)).resize(-> s.resize(this)))(@)
-
-  position: ->
-    @target.css(
-      "position": @state
-      "top": @offset+"px"
-      "left": @targetLeft+"px"
-    )
-
-  resize: ->
-    @targetOffset = parseInt @target.offset().top
-    @targetLeft = parseInt @target.offset().left
-    targetBottom = @targetOffset + @target.height()
-
-    if $(window).height() < targetBottom
-      @disabled = true
-      @position() if @state is FIXED
-    else
-      @disabled = false
-      @scroll()
-
-  scroll: (self) ->
-    return if @disabled
-    if (@targetOffset - $(self).scrollTop()) - @offset <= 0
-      @onStick?() if @state is STATIC
-      @state = FIXED
-    else
-      @onUnstick?() if @state is FIXED
+  class Stick
+    constructor: (@target, args) ->
+      @offset = if typeof args is "number" then args else 0
+      $.extend @, args if typeof args is "object"
       @state = STATIC
-    @position()
+      @resize()
+      ((s) -> $(window).scroll(-> s.scroll(this)).resize(-> s.resize(this)))(@)
+
+    position: ->
+      @target.css(
+        "position": @state
+        "top": @offset+"px"
+        "left": @targetLeft+"px"
+      )
+
+    resize: ->
+      @targetOffset = parseInt @target.offset().top
+      @targetLeft = parseInt @target.offset().left
+      targetBottom = @targetOffset + @target.height()
+
+      if $(window).height() < targetBottom
+        @disabled = true
+        @position() if @state is FIXED
+      else
+        @disabled = false
+        @scroll()
+
+    scroll: (self) ->
+      return if @disabled
+      if (@targetOffset - $(self).scrollTop()) - @offset <= 0
+        @onStick?() if @state is STATIC
+        @state = FIXED
+      else
+        @onUnstick?() if @state is FIXED
+        @state = STATIC
+      @position()
--- jquery.stick.js
+++ jquery.stick.js
@@ -1,83 +1,81 @@
 (function() {
-  var $, FIXED, STATIC, Stick;
 
-  $ = jQuery;
-
-  STATIC = 'static';
-
-  FIXED = 'fixed';
-
-  $.fn.stick = function(args) {
-    var s;
-    return s = new Stick(this, args);
-  };
-
-  Stick = (function() {
-
-    function Stick(target, args) {
-      this.target = target;
-      this.offset = typeof args === "number" ? args : 0;
-      if (typeof args === "object") {
-        $.extend(this, args);
-      }
-      this.state = STATIC;
-      this.resize();
-      (function(s) {
-        return $(window).scroll(function() {
-          return s.scroll(this);
-        }).resize(function() {
-          return s.resize(this);
-        });
-      })(this);
-    }
-
-    Stick.prototype.position = function() {
-      return this.target.css({
-        "position": this.state,
-        "top": this.offset + "px",
-        "left": this.targetLeft + "px"
-      });
+  jQuery(function($) {
+    var FIXED, STATIC, Stick;
+    STATIC = 'static';
+    FIXED = 'fixed';
+    $.fn.stick = function(args) {
+      var s;
+      return s = new Stick(this, args);
     };
+    return Stick = (function() {
 
-    Stick.prototype.resize = function() {
-      var targetBottom;
-      this.targetOffset = parseInt(this.target.offset().top);
-      this.targetLeft = parseInt(this.target.offset().left);
-      targetBottom = this.targetOffset + this.target.height();
-      if ($(window).height() < targetBottom) {
-        this.disabled = true;
-        if (this.state === FIXED) {
-          return this.position();
-        }
-      } else {
-        this.disabled = false;
-        return this.scroll();
-      }
-    };
-
-    Stick.prototype.scroll = function(self) {
-      if (this.disabled) {
-        return;
-      }
-      if ((this.targetOffset - $(self).scrollTop()) - this.offset <= 0) {
-        if (this.state === STATIC) {
-          if (typeof this.onStick === "function") {
-            this.onStick();
-          }
-        }
-        this.state = FIXED;
-      } else {
-        if (this.state === FIXED) {
-          if (typeof this.onUnstick === "function") {
-            this.onUnstick();
-          }
+      function Stick(target, args) {
+        this.target = target;
+        this.offset = typeof args === "number" ? args : 0;
+        if (typeof args === "object") {
+          $.extend(this, args);
         }
         this.state = STATIC;
+        this.resize();
+        (function(s) {
+          return $(window).scroll(function() {
+            return s.scroll(this);
+          }).resize(function() {
+            return s.resize(this);
+          });
+        })(this);
       }
-      return this.position();
-    };
 
-    return Stick;
+      Stick.prototype.position = function() {
+        return this.target.css({
+          "position": this.state,
+          "top": this.offset + "px",
+          "left": this.targetLeft + "px"
+        });
+      };
 
-  })();
-})();
+      Stick.prototype.resize = function() {
+        var targetBottom;
+        this.targetOffset = parseInt(this.target.offset().top);
+        this.targetLeft = parseInt(this.target.offset().left);
+        targetBottom = this.targetOffset + this.target.height();
+        if ($(window).height() < targetBottom) {
+          this.disabled = true;
+          if (this.state === FIXED) {
+            return this.position();
+          }
+        } else {
+          this.disabled = false;
+          return this.scroll();
+        }
+      };
+
+      Stick.prototype.scroll = function(self) {
+        if (this.disabled) {
+          return;
+        }
+        if ((this.targetOffset - $(self).scrollTop()) - this.offset <= 0) {
+          if (this.state === STATIC) {
+            if (typeof this.onStick === "function") {
+              this.onStick();
+            }
+          }
+          this.state = FIXED;
+        } else {
+          if (this.state === FIXED) {
+            if (typeof this.onUnstick === "function") {
+              this.onUnstick();
+            }
+          }
+          this.state = STATIC;
+        }
+        return this.position();
+      };
+
+      return Stick;
+
+    })();
+  });
+
+}).call(this);
